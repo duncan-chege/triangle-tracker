4591a82960ec92c6a7246034d439a5f473239f62
dunyung1 <dshege4@gmail.com>
2018-12-01 22:23:56 +0300

    add output paragraph

--- sides.js
+++ sides.js
@@ -4,13 +4,12 @@ for(var index=0; index<=sides.length; index++){
 }
 
 function triangleType(sides){
- var sideA=parseInt(document.getElementById('txt1').value);  /*value property
- sets or returns the value of the value attribute of a text field*/
- var sideB=parseInt(document.getElementById('txt2').value);
- var sideC=parseInt(document.getElementById('txt3').value);
-
+  var sideA=parseInt(document.getElementById('txt1').value);  /*value property
+  sets or returns the value of the value attribute of a text field*/
+  var sideB=parseInt(document.getElementById('txt2').value);
+  var sideC=parseInt(document.getElementById('txt3').value);
 if(sideA == sideB && sideB == sideC && sideA == sideC){
-  prompt("Equilateral Triangle");
+  prompt("Equilaeral Triangle");
 }else if(sideA==sideB ||sideA==sideC ||sideB==sideC){
   prompt("Isosceles Triangle");
 }else if(sideA!==sideB && sideB!==sideC && sideA!==sideC){
@@ -19,4 +18,5 @@ if(sideA == sideB && sideB == sideC && sideA == sideC){
   prompt("Not a triangle");
 }
 }
+
 //calling the function
