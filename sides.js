79c3de36d1947bbc442a8434511d5ccbd861624f
dunyung1 <dshege4@gmail.com>
2018-12-01 22:25:14 +0300

    add equilateral triangle output

--- sides.js
+++ sides.js
@@ -1,22 +1,23 @@
+
 var sides=[];
 for(var index=0; index<=sides.length; index++){
 //sides.push(parseInt(prompt("Enter the length of sides: "(index+1)))
 }
 
 function triangleType(sides){
-  var sideA=parseInt(document.getElementById('txt1').value);  /*value property
-  sets or returns the value of the value attribute of a text field*/
-  var sideB=parseInt(document.getElementById('txt2').value);
-  var sideC=parseInt(document.getElementById('txt3').value);
-if(sideA == sideB && sideB == sideC && sideA == sideC){
-  prompt("Equilaeral Triangle");
-}else if(sideA==sideB ||sideA==sideC ||sideB==sideC){
-  prompt("Isosceles Triangle");
-}else if(sideA!==sideB && sideB!==sideC && sideA!==sideC){
-  prompt("Scalene Triangle");
+ var sideA=parseInt(document.getElementById("txt1").value);  /*value property
+ sets or returns the value of the value attribute of a text field*/
+ var sideB=parseInt(document.getElementById("txt2").value);
+ var sideC=parseInt(document.getElementById("txt3").value);
+
+if(sideA == sideB== sideC){
+  document.getElementById("result").innerHTML="Equilateral Triangle";
+}else if(sideA==sideB!==sideC ||sideA!==sideB==sideC ||sideA==sideC!==sideB){
+    
+}else if(sideA!==sideB!==sideC){
+
 }else{
   prompt("Not a triangle");
 }
 }
-
 //calling the function
