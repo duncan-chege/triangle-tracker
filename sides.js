7535a2602dbbaa4cc309ccef29e2ea648a023ef3
dunyung1 <dshege4@gmail.com>
2018-12-01 13:56:33 +0300

    add initial files

---
+++ sides.js
@@ -0,0 +1,22 @@
+var sides=[];
+for(var index=0; index<=sides.length; index++){
+//sides.push(parseInt(prompt("Enter the length of sides: "(index+1)))
+}
+
+function triangleType(sides){
+ var sideA=parseInt(document.getElementById('txt1').value);  /*value property
+ sets or returns the value of the value attribute of a text field*/
+ var sideB=parseInt(document.getElementById('txt2').value);
+ var sideC=parseInt(document.getElementById('txt3').value);
+
+if(sideA == sideB && sideB == sideC && sideA == sideC){
+  prompt("Equilateral Triangle");
+}else if(sideA==sideB ||sideA==sideC ||sideB==sideC){
+  prompt("Isosceles Triangle");
+}else if(sideA!==sideB && sideB!==sideC && sideA!==sideC){
+  prompt("Scalene Triangle");
+}else{
+  prompt("Not a triangle");
+}
+}
+//calling the function
