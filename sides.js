8cd1d60d06ed8c637c07ec157c447c23ddee84f3
dunyung1 <dshege4@gmail.com>
2018-12-02 07:24:45 +0300

    change triangle conditions

--- sides.js
+++ sides.js
@@ -1,23 +1,20 @@
 
-var sides=[];
-for(var index=0; index<=sides.length; index++){
-//sides.push(parseInt(prompt("Enter the length of sides: "(index+1)))
-}
 
-function triangleType(sides){
+function triangleType(){
  var sideA=parseInt(document.getElementById("txt1").value);  /*value property
  sets or returns the value of the value attribute of a text field*/
  var sideB=parseInt(document.getElementById("txt2").value);
  var sideC=parseInt(document.getElementById("txt3").value);
 
-if(sideA == sideB== sideC){
+if(sideA+sideB<=sideC || sideC+sideB<=sideA || sideA+sideC<=sideB){
+  document.getElementById("result").innerHTML="Not a Triangle";
+}else if(sideA == sideB && sideB == sideC){
   document.getElementById("result").innerHTML="Equilateral Triangle";
-}else if(sideB!==sideC==sideA || sideA!==sideB==sideC || sideC!==sideB==sideA){
+}else if(sideB==sideC || sideA==sideC|| sideA==sideB){
   document.getElementById("result").innerHTML="Isosceles Triangle";
-}else if(sideA!==sideB!==sideC){
-  document.getElementById("result").innerHTML="Scalene Triangle";
 }else{
-  prompt("Not a triangle");
+  document.getElementById("result").innerHTML="Scalene Triangle";
 }
 }
+
 //calling the function
