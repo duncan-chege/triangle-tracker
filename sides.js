73f04d042720a8c89485a776dca2b4f3c1754d1d
dunyung1 <dshege4@gmail.com>
2018-12-06 10:18:23 +0300

    alter triangle tracker conditions

--- sides.js
+++ sides.js
@@ -1,25 +1,29 @@
 
-function triangleType(sideX,sideY,sideZ){
- var sideX=parseInt(document.getElementById("txt1").value);  /*value property
- sets or returns the value of the value attribute of a text field*/
- var sideY=parseInt(document.getElementById("txt2").value);
- var sideZ=parseInt(document.getElementById("txt3").value);
-
+function triangleType(){   //try without the arguments
  var sides=[];
- sides.push(sideX,sideY,sideZ);
- var x=sides[0];
- var y=sides[1];
- var z=sides[2];
+ sides.push(document.getElementById("txt1").value);  /*value property
+ sets or returns the value of the value attribute of a text field*/
+ sides.push(document.getElementById("txt2").value);
+ sides.push(document.getElementById("txt3").value);
 
-if(x+y<=z || x+y<=z || x+y<=z){
-  document.getElementById("result").innerHTML="Not a Triangle";
-}else if(x==y && y==z){
-  document.getElementById("result").innerHTML="Equilateral Triangle";
-}else if(y==z || x==z|| x==y){  //chain your comparisons together with logical operators,
-  document.getElementById("result").innerHTML="Isosceles Triangle";
+if(sides[0]==""||sides[1]==""||sides[2]==""){
+  document.getElementById("result").innerHTML="Please enter values";
 }else{
-  document.getElementById("result").innerHTML="Scalene Triangle";
+  var x=parseInt(sides[0]);
+  var y=parseInt(sides[1]);
+  var z=parseInt(sides[2]);
+  if(x+y<=z || y+z<=x || x+z<=y){
+   document.getElementById("result").innerHTML="Not a Triangle";
+ }else if(x==y && y==z){
+   document.getElementById("result").innerHTML="Equilateral Triangle";
+ }else if(y==z || x==z|| x==y){  //chain your comparisons together with logical operators,
+   document.getElementById("result").innerHTML="Isosceles Triangle";
+ }else{
+   document.getElementById("result").innerHTML="Scalene Triangle";
+ }
 }
 }
+
+
 
 //calling the function
